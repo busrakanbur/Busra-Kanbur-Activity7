6def0fa0b15cb15421afdecb2ce84d48a3cf15a4
busrakanbur <nolensvolens99@gmail.com>
2023-05-16 22:57:22 +0300

    js file edit

--- Kanbur-Activity7.js
+++ Kanbur-Activity7.js
@@ -10,11 +10,9 @@ window.onload = function () {
 	$("display_scores").onclick = displayScores;
 	$("add").onclick = addScore;
   
-	// Set focus to the Name field on page load
 	$("name").focus();
   };
   
-
 
 
 window.onload = function () {
@@ -25,33 +23,20 @@ window.onload = function () {
 	
 };
 
-// function displayResults()
-// {
-// 	var average = 0;
-// 	for(var i=0;i<scores.length;i++)
-// 	{
-// 		average= (average*(i)+scores[i])/(i+1);
-// 	}
-	
-// 	document.getElementById("results").innerHTML="<h2> Results </h2><br /> Average score is "+average + "<br \> "
-// }
 function displayResults() {
 	var average = 0;
 	var highestScore = 0;
   
-	// Calculate the average score
 	for (var i = 0; i < scores.length; i++) {
 	  average = (average * i + scores[i]) / (i + 1);
 	}
   
-	// Find the highest score
 	for (var j = 0; j < scores.length; j++) {
 	  if (scores[j] > highestScore) {
 		highestScore = scores[j];
 	  }
 	}
   
-	// Display the results in the 'results' div element
 	var resultsDiv = document.getElementById("results");
 	resultsDiv.innerHTML =
 	  "<h2>Results</h2>" +
@@ -62,16 +47,16 @@ function displayResults() {
 
   function displayScores() {
 	var table = document.getElementById("scores_table");
-	table.innerHTML = ""; // Clear any existing rows in the table
+	table.innerHTML = "";
   
-	// Create table header row
+
 	var headerRow = table.insertRow(0);
 	var nameHeader = headerRow.insertCell(0);
 	nameHeader.textContent = "Name";
 	var scoreHeader = headerRow.insertCell(1);
 	scoreHeader.textContent = "Score";
   
-	// Insert rows for each name and score
+
 	for (var i = 0; i < names.length; i++) {
 	  var row = table.insertRow(i + 1);
 	  var nameCell = row.insertCell(0);
@@ -85,21 +70,21 @@ function displayResults() {
 	var nameInput = $("name").value;
 	var scoreInput = parseInt($("score").value);
   
-	// Validate the inputs
+
 	if (nameInput.trim() === "" || isNaN(scoreInput) || scoreInput < 0 || scoreInput > 100) {
 	  alert("You must enter a name and a valid score (0-100).");
 	  return;
 	}
   
-	// Add the name and score to the arrays
+
 	names.push(nameInput);
 	scores.push(scoreInput);
   
-	// Clear the input fields
+	//clear inputs
 	$("name").value = "";
 	$("score").value = ""
   
-	// Move the cursor to the Name field after adding a score
+
 	$("name").focus();
   
   }
